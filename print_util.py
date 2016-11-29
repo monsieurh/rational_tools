d8cebcdb396070b1b25e6b58d549aa4d254345de
Hubert Ray <hubert.ray@gumi-europe.com>
2016-11-29 11:53:14 +0100

    tweaks printing lib

--- print_util.py
+++ print_util.py
@@ -1,12 +1,13 @@
 import os
+import textwrap
 
 from termcolor import colored
 
 try:
     term_w, _ = os.get_terminal_size()
 except OSError:
-    term_w = 40
-APP_WIDTH = min(40, term_w)
+    term_w = 45
+APP_WIDTH = min(45, term_w)
 
 
 class GenericPrinter:
@@ -27,10 +28,10 @@ class GenericPrinter:
         if not attrs:
             attrs = ['bold']
 
-        left = key
-        right = value
-        padding = "{}".format("".rjust(APP_WIDTH - (len(str(left)) + len(str(right))), " "))
+        left = str(key) + ':'
+        right = str(value)
 
         left = colored(left, color=color, attrs=attrs)
-        right = colored(right, color=color, attrs=attrs)
-        print(left, padding, right, sep="")
+        right = colored(right, color=color)
+        print(textwrap.fill(left, APP_WIDTH))
+        print(textwrap.fill('\n\t' + right, APP_WIDTH))
